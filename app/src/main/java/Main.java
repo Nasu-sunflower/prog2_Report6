a8d1a481be1df41e89cd2484602f827354efd370
Tairin Nasu <e225406@ie.u-ryukyu.ac.jp>
2024-02-03 15:11:09 +0900

    this is 6th use gradle

---
+++ app/src/main/java/Main.java
@@ -0,0 +1,47 @@
+import jp.ac.uryukyu.ie.e225406.*;
+// package main.jp.ac.uryukyu.ie.e225406;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    
+
+    public static void main(String[] args) {
+
+    int playHand;
+    String input;
+        
+    Scanner scanner = new Scanner(System.in); // scannerをここで初期化している
+
+    GirlFrend girlfrend = new GirlFrend("あつこ", 100, false);
+
+    System.out.printf("今日%sさんと出会った。素敵な人だ\n",girlfrend.getName());
+
+    int turn = 0;
+        while(  girlfrend.getMarriage() == false ){
+            turn++;
+            System.out.printf("出会って%d日目！\n今日はどうやってアピールしよう\n", turn);
+            
+            do {
+                System.out.println("1:おしゃべりする 2:プレゼントを渡す 3:プロポーズする");
+
+                input = scanner.next(); //コマンド画面から文字列の入力を読み取ります
+
+                if (input.matches("[1-3]")) {//1~3の場合はtrue
+
+                    playHand = Integer.parseInt(input); // 文字列の入力を整数型に置き換えて代入します
+
+                    break; //ループを抜けます
+                }
+                System.out.println("該当する半角数字で入力してください");
+            } while (true);
+
+            if(playHand == 1){girlfrend.talk(girlfrend);}
+            if(playHand == 2){girlfrend.doPresent(girlfrend);}
+            if(playHand == 3){girlfrend.propose(girlfrend);}
+        }
+        System.out.printf("末永くお幸せに！\n%d日で結婚までたどり着いた！",turn);
+    }
+}
---
+++ app/src/main/java/jp/ac/uryukyu/ie/e225406/GirlFrend.java
@@ -0,0 +1,57 @@
+package jp.ac.uryukyu.ie.e225406;
+
+public class GirlFrend {
+
+    private String name;
+    private int love;
+    private boolean marriage;
+
+    public GirlFrend(String name,int maximumlove,boolean marriage){
+        this.name = name;
+        love = maximumlove;
+        marriage = false;
+    }
+
+    //getter
+    public String getName(){return this.name;}
+    public int getLove(){return this.love;}
+    public boolean getMarriage(){return this.marriage;}
+
+    public void doPresent(GirlFrend girlFrend){
+
+        love -= 30;
+
+        System.out.printf("%sさん、プレゼントです！\n", name);
+
+        if (love < 0){
+            System.out.printf("%s:あなたとずっと一緒にいたいわ。\n", name);
+        }else{ 
+            System.out.printf("%s:うれしいわ。\n", name);
+        }
+    }
+
+    public void talk(GirlFrend girlFrend){
+
+        love -= 20;
+
+        System.out.printf("%sさん、おしゃべりしましょう！\n", name);
+
+        if (love < 0){
+            System.out.printf("%s:あなたとずっと一緒にいたいわ。\n", name);
+        }else{ 
+            System.out.printf("%s:たのしいわ。\n", name);
+        }
+    }
+
+    public void propose(GirlFrend girlFrend){
+
+        System.out.printf("僕と結婚してください！\n");
+        
+        if(love <= 0){
+            System.out.printf("%s:よろこんで！\n", name);
+            marriage = true;
+        }else{
+            System.out.printf("%s:あなたのことそういう風に見れないのごめんなさいね。\n", name);
+        }
+    }
+}
